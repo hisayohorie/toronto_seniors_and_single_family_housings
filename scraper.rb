8499791a8af225aade7d27b0413418c03318bb45
hisayohorie <hisayohorie@gmail.com>
2017-08-22 19:55:06 -0400

    Created the basic scraper to scrape a row in the table

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,19 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("https://www.torontohousing.ca/about/our-housing/Pages/Seniors-and-single-family-homes-(East).aspx")
 #
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
+page.search("tr").each do |tr|
+  puts tr.search("td").first.text
+end
 #
 # # Write out to the sqlite database using scraperwiki library
 # ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
